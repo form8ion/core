bfa3580b98d569e3d98b88861d08854d531ad7fa
Matt Travi <programmer@travi.org>
2022-05-20 16:15:27 -0500

    test(config-file): backfilled integration tests for writing json and yaml config files

---
+++ test/integration/features/config-file.feature
@@ -0,0 +1,11 @@
+Feature: config file
+
+  Scenario: write json config
+    Given the desired config file format is "json"
+    When the config file is written
+    Then the config is defined in the file
+
+  Scenario: write yaml config
+    Given the desired config file format is "yaml"
+    When the config file is written
+    Then the config is defined in the file
---
+++ test/integration/features/step_definitions/config-file-steps.js
@@ -0,0 +1,58 @@
+import {resolve} from 'path';
+import {promises as fs} from 'fs';
+import {load} from 'js-yaml';
+// eslint-disable-next-line import/no-extraneous-dependencies,import/no-unresolved
+import {writeConfigFile} from '@form8ion/core';
+
+import {After, Before, Given, Then, When} from '@cucumber/cucumber';
+import {assert} from 'chai';
+import stubbedFs from 'mock-fs';
+import any from '@travi/any';
+import {fileTypeExtensions, fileTypes} from '../../../../src/file-types';
+
+const pathToProjectRoot = [__dirname, '..', '..', '..', '..'];
+const pathToNodeModules = [...pathToProjectRoot, 'node_modules'];
+const stubbedNodeModules = stubbedFs.load(resolve(...pathToNodeModules));
+
+function parseConfigFileContent(fileContents, format) {
+  if (fileTypes.JSON === format) return JSON.parse(fileContents);
+  if (fileTypes.YAML === format) return load(fileContents);
+
+  throw new Error('desired file format is unsupported, so not parsing');
+}
+
+Before(function () {
+  stubbedFs({
+    node_modules: stubbedNodeModules
+  });
+});
+
+After(function () {
+  stubbedFs.restore();
+});
+
+Given('the desired config file format is {string}', async function (format) {
+  this.desiredConfigFileFormat = format;
+});
+
+When('the config file is written', async function () {
+  this.config = any.simpleObject();
+  this.configName = any.word();
+
+  await writeConfigFile({
+    format: this.desiredConfigFileFormat,
+    config: this.config,
+    path: process.cwd(),
+    name: this.configName
+  });
+});
+
+Then('the config is defined in the file', async function () {
+  const {desiredConfigFileFormat} = this;
+  const fileContents = await fs.readFile(
+    `${process.cwd()}/${this.configName}.${fileTypeExtensions[desiredConfigFileFormat]}`
+  );
+  const config = parseConfigFileContent(fileContents, desiredConfigFileFormat);
+
+  assert.deepEqual(config, this.config);
+});
