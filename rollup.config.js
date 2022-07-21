3bb760581b346eca95bc2a5e0fc0fa6202494a61
Matt Travi <programmer@travi.org>
2022-07-21 08:53:17 -0500

    feat(node-versions): defined supported node ranges
    BREAKING CHANGE: supported node ranges have been defined, limiting to v14 above v14.18 and above v16

--- rollup.config.js
+++ rollup.config.js
@@ -9,7 +9,7 @@ export default {
     babel({
       babelrc: false,
       exclude: ['./node_modules/**'],
-      presets: [['@form8ion', {targets: {node: '10'}, modules: false}]]
+      presets: [['@form8ion', {targets: {node: '14.18'}, modules: false}]]
     })
   ],
   external: 'spdx-license-list/simple',
