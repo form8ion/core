248b4bfa4af1946f1583ee022f47648baa9c666c
Matt Travi <programmer@travi.org>
2024-01-12 23:14:55 -0600

    feat(file-types): added definitions for javascript types

--- src/file-types.js
+++ src/file-types.js
@@ -1,9 +1,15 @@
 export const fileTypes = {
   JSON: 'json',
-  YAML: 'yaml'
+  YAML: 'yaml',
+  COMMON_JS: 'common-js',
+  ES_MODULE: 'es-module',
+  JAVASCRIPT: 'javascript'
 };
 
 export const fileTypeExtensions = {
   [fileTypes.JSON]: 'json',
-  [fileTypes.YAML]: 'yml'
+  [fileTypes.YAML]: 'yml',
+  [fileTypes.COMMON_JS]: 'cjs',
+  [fileTypes.ES_MODULE]: 'mjs',
+  [fileTypes.JAVASCRIPT]: 'js'
 };
